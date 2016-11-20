7edb03b2e8858de5c23ecd337eecc8c8c4ff1e8e
aisflat439 <fitzsimonsdevin@gmail.com>
2016-11-20 13:54:11 -0500

    updates

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 
-app.set('port', (process.env.PORT || 500))
+app.set('port', (process.env.PORT || 5000))
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 
-app.get('/', function(req, res){
-  res.send("OMGGMO")
+app.get('/', function (req, res) {
+    res.send('OMGGMO')
 })
 
 
 
 app.get('/webhook', function(req, res){
-  if(req.query['hub.verify_token'] ===
-    'my_voice_is_my_password_verify_me'){
+  if(req.query['hub.verify_token'] === 'myfirstsampleappone'){
       res.send(req.query['hub.challenge'])
     }
   res.send('No Entry')
