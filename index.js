c8513a53422f3350006a9dfdbb58ef9db7c14350
aisflat439 <fitzsimonsdevin@gmail.com>
2016-11-23 11:23:09 -0500

    Added Seven function

--- index.js
+++ index.js
@@ -66,6 +66,22 @@ app.post('/webhook', function (req, res) {
 function evaluatePostback(event){
   console.log("-----------Something happened------------");
   console.log(event.postback);
+
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var postback = event.postback
+
+  if (postback){
+
+    switch (postback) {
+      case 'One':
+
+        break;
+      default:
+        sendSeven(senderID);
+        break;
+    }
+  }
 }
 
 function receivedMessage(event) {
@@ -92,10 +108,6 @@ function receivedMessage(event) {
         buttonReply(senderID);
         break;
 
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
       case 'hello':
         sendHelloWorld(senderID);
         break;
@@ -118,53 +130,6 @@ function receivedMessage(event) {
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
   }
-}
-
-function sendGenericMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
 }
 
 function sendTestButton(recipientId){
@@ -268,6 +233,19 @@ function sendHelloWorld(recipientId) {
   };
 
   callSendAPI(messageData);
+}
+
+function sendSeven(recipientId){
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: "SEVENS!"
+      }
+    };
+
+    callSendAPI(messageData);
 }
 
 function sendToots(recipientId){
