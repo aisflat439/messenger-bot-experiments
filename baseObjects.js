9766f0558eb00eb70cd31910cb0e13087f08faaa
aisflat439 <fitzsimonsdevin@gmail.com>
2016-11-27 15:20:58 -0500

    attempt at button

--- baseObjects.js
+++ baseObjects.js
@@ -7,4 +7,35 @@ var textMessage = {
   }
 };
 
+var threeButtonMessage = {
+      recipient: {
+        id: ""
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "button",
+            text: "",
+            buttons:[{
+              type: "postback",
+              title: "",
+              payload: ""
+            }, {
+              type: "postback",
+              title: "",
+              payload: ""
+            }, {
+              type: "postback",
+              title: "",
+              payload: ""
+            }]
+          }
+        }
+      }
+    };
+
+}
+
 exports.textMessage = textMessage;
+exports.threeButtonMessage = threeButtonMessage;
