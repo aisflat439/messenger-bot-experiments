d8d230f6ac881a6ab17890b87cddc1911b4cc251
aisflat439 <fitzsimonsdevin@gmail.com>
2016-12-31 14:45:45 -0500

    trying to seperate out photos

---
+++ photoMessageObjects.js
@@ -0,0 +1,19 @@
+const SERVER_URL = "https://desolate-meadow-32257.herokuapp.com"
+
+function PhotoOnlyMessage(id, photoURL){
+  this.message = {
+    recipient: {
+      id: id
+    },
+    message: {
+      attachment: {
+        type: "image",
+        payload: {
+          url: SERVER_URL + photoURL
+        }
+      }
+    }
+  };
+}
+
+exports.PhotoOnlyMessage = PhotoOnlyMessage;
